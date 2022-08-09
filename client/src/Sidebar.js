051e4a5f0075620bb3783561277cd8442788fe82
shannonshao <90943847+shannonshao@users.noreply.github.com>
2022-08-09 03:03:06 -0700

    Small style changes for merged branches

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -6,16 +6,20 @@ import './Sidebar.css';
 export default props => {
   return (
     <Menu>
-      <a className="menu-item" href="/">
+      <a className="menu-item" href="/home">
+        <img src="https://img.icons8.com/ultraviolet/20/000000/home--v1.png" alt="Home Icon"/>
         Home
       </a>
-      <a className="menu-item" href="/salads">
+      <a className="menu-item" href="/profile">
+        <img src="https://img.icons8.com/ultraviolet/20/000000/test-account.png" alt="Profile Icon"/>  
         Profile
       </a>
-      <a className="menu-item" href="/pizzas">
+      <a className="menu-item" href="/cart">
+      <img src="https://img.icons8.com/ultraviolet/20/000000/shopping-cart.png" alt="Shopping Cart Icon"/>
         Cart
       </a>
-      <a className="menu-item" href="/desserts">
+      <a className="menu-item" href="/categories">
+      <img src="https://img.icons8.com/ultraviolet/20/000000/shirt.png" alt="Categories Icon"/>
         Categories
       </a>
     </Menu>
